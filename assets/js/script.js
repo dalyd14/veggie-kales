51247541b9b2c085776ac7c10e6e792399283524
David Daly <dalyd14@gmail.com>
2020-11-27 09:13:36 -0500

    on card click it opens the recipe page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var recipeDisplay = $("#recipes-row")
 
 for(var i = 0; i < recipes.length; i++) {
-    recipeCard = $("<div>").addClass("card mb-2")
+    recipeCol = $("<div>").addClass("col-12 col-md-6")
+    recipeCard = $("<div>").addClass("card mb-2").attr("id", i)
     recipeCardImg = $("<img>").addClass("card-img-top").attr("src",recipes[i].mainImg)
     recipeCardBody = $("<div>").addClass("card-body")
     recipeCardBodyTitle = $("<h5>").addClass("card-title").text(recipes[i].title)
@@ -9,5 +10,10 @@ for(var i = 0; i < recipes.length; i++) {
 
     recipeCardBody.append(recipeCardBodyTitle, recipeCardBodyDescr)
     recipeCard.append(recipeCardImg, recipeCardBody)
-    recipeDisplay.append(recipeCard)
+    recipeCol.append(recipeCard)
+    recipeDisplay.append(recipeCol)
 }
+
+$("#recipes-container").on("click", ".card", function(){
+    window.location = "./recipes.html?" + $(this).attr("id")
+})
